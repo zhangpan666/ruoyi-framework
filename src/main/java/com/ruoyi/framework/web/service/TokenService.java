fc89be52a4e5b5f93defb0715853d48ca93af808
xiaojie <xiaojie@gmail.com>
2026-03-02 19:05:56 +0800

    update

--- src/main/java/com/ruoyi/framework/web/service/TokenService.java
+++ src/main/java/com/ruoyi/framework/web/service/TokenService.java
@@ -63,6 +63,16 @@ public class TokenService
     {
         // 获取请求携带的令牌
         String token = getToken(request);
+        return getLoginUser(token);
+    }
+
+    /**
+     * 获取用户身份信息
+     *
+     * @return 用户信息
+     */
+    public LoginUser getLoginUser(String token)
+    {
         if (StringUtils.isNotEmpty(token))
         {
             try
